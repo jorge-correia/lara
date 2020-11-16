698acd648362f06270ccdf8b2208384b8b5023fb
jorge_correia <correia.jorge.jc@protonmail.com>
2020-11-16 11:19:04 +0000

    static home page

---
+++ frontend/public/component---src-pages-index-js-81131ad0f36f2486ac89.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{RXBc:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return o}));var a=t("q1tI"),c=t.n(a),l=t("Wbzz"),u=t("W/9C"),r=t("RMg3");t("HY6h"),t("VxdY");function o(){return c.a.createElement("div",null,c.a.createElement(r.a,null),c.a.createElement(l.a,{to:"/publicacoesPage"},"Hi lorena"),c.a.createElement(u.a,null))}},VxdY:function(e,n,t){}}]);
+//# sourceMappingURL=component---src-pages-index-js-81131ad0f36f2486ac89.js.map
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,14 +2,50 @@ import React from "react";
 import { Link } from "gatsby"
 import Footer from '../components/footer';
 import NavBar from '../components/navbar'
+import '../styles/estilo.css';
+import '../styles/index.css';
 
 
 export default function Home() {
   return( 
     <div>
-    <NavBar/>
-    <Link to="/publicacoesPage">Hi lorena</Link>
-    <Footer/>
+      <div>
+        <NavBar/>
+        <Link to="/publicacoesPage">Hi lorena</Link>
+        <Footer/>
+      </div>
+
+
+      <div className="container sobre-lara">
+        <h2 className="titulo-lara">Laboratório Remoto em AVA</h2>
+        // lara
+      </div>
+
+      
+      <div className="container experimento">
+        <h2 className="titulo">Experimentos</h2>
+        // O TITULO N TA APARECENDO PQ A COR DA FONTE EH BRANCA 
+        // exps
+      </div>
+
+
+      <div className="container sobre-colaboracao">
+        <h2 className="titulo2">Colaboração</h2>
+        // colab
+      </div>
+
+
+      <div className="container sobre-multilinguagem">
+        <h2 className="titulo">Multilinguagem</h2>
+        // O TITULO N TA APARECENDO PQ A COR DA FONTE EH BRANCA 
+        // multilinguagem
+      </div>
+
+
+      <div className="container sobre-contato">
+        <h2 className="titulo2">Entre em contato</h2>
+        // contato
+      </div>
     </div>
   );
   }
