5701713e09cd23a2a953b8db19bccdecd6b9533a
jorge_correia <correia.jorge.jc@protonmail.com>
2020-11-18 17:23:03 +0000

    adaptacao do banco de dados ao strapi

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f539f8f6d588428d34802145ae69216a'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1889627d-61b9-40fa-9819-ec0f75d31c5a'
+};
