922336a366b10fd3116c3c061862a8c00bcd0814
joaoandreotti <joao.andreotti@hotmail.com>
2020-11-30 23:03:51 +0000

    dumb migration v2

--- strapi/scripts/mariadb_migration.sh
+++ strapi/scripts/mariadb_migration.sh
@@ -5,8 +5,35 @@ then
 	echo 'USAGE: ./migration_insert.sh USERNAME PASSWORD DATABASE_NAME'
 	echo 'EXAMPLE: ./migration_insert.sh user pass strapi'
 else
+	echo '[*] Starting migration'
 	mysqldump --user=$1 --password=$2 --databases $3 > tmp.sql
-	echo $(cat tmp.sql | grep 'INSERT.*;') > ../sql_sample/lara_database_inserts.sql
-	echo $(cat tmp.sql | sed -e 's/INSERT.*//g') > ../sql_sample/lara_database_creation.sql
+
+
+	echo '[*] Generating inserts'
+	rm ../sql_sample/lara_database_inserts.sql
+	while IFS= read -r line;
+	do
+		str=$(echo $line | grep 'INSERT.*;')
+		if [ $(echo $str | wc -c) -gt 1 ]
+		then
+			echo $str >> ../sql_sample/lara_database_inserts.sql
+		fi
+	done < tmp.sql
+	echo '[+] Done inserts'
+
+	echo '[*] Generating database'
+	rm ../sql_sample/lara_database_creation.sql
+	while IFS= read -r line;
+	do
+		str=$(echo $line | sed -e 's/INSERT.*//g')
+		if [ $(echo $str | wc -c) -gt 1 ]
+		then
+			echo $str >> ../sql_sample/lara_database_creation.sql
+		fi
+	done < tmp.sql
+	echo '[+] Done database'
+
+	echo '[*] Cleaning'
 	rm tmp.sql
+	echo '[+] Done cleaning'
 fi
