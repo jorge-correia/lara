f3e2bb281b02353a06fe924178cb3bbaa1e50218
Jang <jjangj@gmail.com>
2020-11-30 20:43:05 -0300

    teste

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
     <Layout>
       <DetalhePag />
       <ExperimentosIndex />
-      <Teste1 comDiv={true} id="secao3" titulo="Colaboração" container="container sobre-colaboracao" desc="desc-colaboracao" conteudo="O LARA permite que os alunos possam resolver problemas e programar em equipes de modo que todos consigam interagir entre si e ver as mudanças que os colegas estão fazendo no trabalho, além de permitir que os alunos possam pedir ajuda a monitores e professores." image="img-colab" srcImage={Colaboracao} /> 
+      <Teste1 comDiv={true} id="secao3" titulo="Colaboração" container="container sobre-colaboracao" desc="desc-colaboracao" conteudo="O LARA permite qqqque os alunos possam resolver problemas e programar em equipes de modo que todos consigam interagir entre si e ver as mudanças que os colegas estão fazendo no trabalho, além de permitir que os alunos possam pedir ajuda a monitores e professores." image="img-colab" srcImage={Colaboracao} /> 
       <Teste2 id="secao4" titulo="Multilinguagem" container="container sobre-multilinguagem" image="img-mult" srcImage={Multilinguagem} conteudo="Atualmente, os experimentos do LARA podem ser programados em C++ e Python para melhor satisfazer professores e alunos. E futuramente pretendemos abranger muitas outras linguagens de programação  e outras formas de programação como em bloco."/>
       <Teste1 id="secao5" titulo="Entre em contato" container="container sobre-contato" desc="desc-contato" conteudo="Gostou do nosso projeto e gostaria de entrar em contato? Nos envie um email para o endereço lara@uesb.edu.br" image="img-contact" srcImage={Contato} />
     </Layout>
