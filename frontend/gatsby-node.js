e61d1dcde120533b16aeb0ebc42c460955a640cc
jjangj <you@example.com>
2020-12-15 10:25:00 -0300

    testando

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -1,19 +1,3 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
-
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
-/*
 exports.sourceNodes=({ actions}) =>{
     const{ createTypes} = actions;
     const typeDefs= `
@@ -21,8 +5,49 @@ exports.sourceNodes=({ actions}) =>{
         tipoPublicacao: String
         refPublicacao: String
     }
-    
     `
 
     createTypes(typeDefs);
-}*/
+}
+
+/**  
+ ** Deixei algumas coisas prontas aqui, so nao sei qual o tipo coloca para imagens 
+
+type strapiExperimentos implements Node{
+      //fotoExperimento: image/midia
+      linkExperimento: String
+      nomeExperimento: String
+      descricaoExperimento: String
+    }
+
+    type strapiCursos implements Node{
+      nomeCurso: String
+      descricaoCurso: String
+      //fotoCurso: image/midia
+      linkCurso: String
+    }
+
+    type strapiProfessores implements Node{
+      nomeProfessor: String
+      descricaoProfessor: String
+      fotoProfessor: image
+      lattesProfessor: String
+    }
+
+    type strapiAlunos implements Node{
+      nomeAluno: String
+      descricaoAluno: String
+      lattesAluno: String
+      linkedinAluno: String
+    }
+
+    type strapiPostBlog implements Node{
+      tituloPost: String
+      conteudoPost: String?
+      fotoPost: image?
+      dataPublicacaoPost: date
+      autores: relation?
+    }
+
+*/ 
+ 
