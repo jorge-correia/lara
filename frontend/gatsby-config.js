757cd3c370324f288e9b1ccceba118a828fb2646
jjangj <you@example.com>
2020-12-22 11:46:40 -0300

    font arrumada

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,28 +1,30 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
   plugins: [      
-      {
-          resolve: `gatsby-plugin-google-fonts`,
+        {
+            resolve: "gatsby-plugin-web-font-loader",
+            options: {
+                custom: {
+                families: ["Roboto, Regular"],
+                urls: ["/fonts/fonts.css"],
+                },
+            },
+        },
+        {
+          resolve: "gatsby-plugin-google-fonts",
           options: {
               fonts: [
-                  `Roboto:300`       
-              ], display: 'swap'       
+                  "Roboto:300" 
+              ], display: "swap"       
           }
-      }
-  ],
-  plugins: [
-    {
-      resolve: `gatsby-source-strapi`,
-      options: {
-        apiURL: process.env.API_URL || `http://localhost:1337`,
-        contentTypes: ["publicacoes","card-alunos", "card-professores", "cursos", "experimentos"],
-        queryLimit: 1000,
-      },
-    },
-  ],
+        },
+        {
+            resolve: "gatsby-source-strapi",
+            options: {
+                apiURL: process.env.API_URL || "http://localhost:1337",
+                contentTypes: ["publicacoes","card-alunos", "card-professores", "cursos", "experimentos"],
+                queryLimit: 1000,
+            },
+        },
+    ],
+
 }
